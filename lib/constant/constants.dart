6a830dbf8545bb5b9bc062431954285ea9652d45
Supreetha-GS <89566928+Supreetha-GS@users.noreply.github.com>
2023-07-20 09:32:48 +0530

    first commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,16 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQ5ZGE5NTQ2MDIxMDVCYWNiNjlFNzMwNjk0NzM1NzM4ZDQ0NjFmMTciLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4MDk0OTAzMTI4OSwibmFtZSI6ImxhbmRfcmVnaXN0cnkifQ.kRyisS472wjDYqasS6bTpJYZqLOSOF5gW9wQLD4uoEI';
+
+const String rpcUrl ="https://rpc-mumbai.maticvigil.com/v1/27ee6b60677f4615a2733c0cf6e3f6096f72891a";
+
+//"http://127.0.0.1:7545";
+
+    
+
+
+const String contractAddress = "0x3dC9507eCB08cB6A5621d74757b95be1626cb35d";//"0xA3bF20a83863213cd9711B31cd11C43228fD0D79"; //
+
+const int chainId = 80001; //Polygon testnet-80001, local ganache-1337
